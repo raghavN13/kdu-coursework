50f48c651baee8ed5a256b5079fee3cc79e395cf
raghavnandwana <raghav.nandwana@kickdrumtech.com>
2024-01-09 00:05:45 +0530

    Add homework-1

---
+++ backend/homework/1/javaFundamentalsPart1/Module1-core/src/main/java/org/javaFundamentals/Student/Student.java
@@ -0,0 +1,41 @@
+package org.javaFundamentals.Student;
+
+
+public class Student {
+    public int id;
+    public String name;
+    public int age;
+    public String grade;
+    public Student(){
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
