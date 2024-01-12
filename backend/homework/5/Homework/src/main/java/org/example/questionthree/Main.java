8d4fb25c2ed87e9781e93ad68636e3b7f5bf26df
raghavnandwana <raghav.nandwana@kickdrumtech.com>
2024-01-13 04:26:59 +0530

    add changes

--- backend/homework/5/Homework/src/main/java/org/example/questionthree/Main.java
+++ backend/homework/5/Homework/src/main/java/org/example/questionthree/Main.java
@@ -5,7 +5,7 @@ import org.example.Logging;
 public class Main {
     int number = 8;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Main shared = new Main();
         Thread threadOne = new Thread(new Factorial(shared));
         Thread threadTwo = new Thread(new Factors(shared));
